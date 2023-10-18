9ae0641af37e0516cfa677ab15da535a0f65b600
mikic202 <105045957+mikic202@users.noreply.github.com>
2023-10-18 20:59:18 +0200

    Add files via upload

---
+++ zad2b.m
@@ -0,0 +1,12 @@
+possible_u = [1.2:0.01:2.8];
+stat_y = [];
+for i= 1:size(possible_u, 2)
+    y_step = step_simulation(possible_u(i), sim_end);
+    stat_y(i) = y_step(end);
+end
+
+plot(possible_u, stat_y)
+title('Zależność wyjścia od sterowania')
+xlabel('Sterowanie')
+ylabel('Wyjście')
+matlab2tikz ('zad2_stat.tex' , 'showInfo' , false)
