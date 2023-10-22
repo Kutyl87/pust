69c30fd50d660a1704dc14b40b760c08db31824d
pkutyl <01168894@pw.edu.pl>
2023-10-22 18:47:25 +0200

    Add zad6

--- dmcfunction.m
+++ dmcfunction.m
@@ -1,43 +1,4 @@
-%% Parametry zadania
-Upp = 2;
-Ypp = 0.8;
-Umin= 1.2;
-Umax = 2.8;
-deltaumax = 0.07;
-
-%% Parametry regulatora DMC
-D =140;
-N =32;
-Nu =29;
-lambda = 13;
-yzad(1:260)=0.9;
-%% Wartość zadana
-kk=1001;
-yzad(1:260)=0.9;
-yzad(261:451)= 0.67;
-yzad(452:762) = 0.7;
-yzad(763:898) = 0.98;
-yzad(899:kk) = 0.59;
-[u, y, e] = regulator_DMC(Upp, Ypp,s, yzad, D, N, Nu, lambda, deltaumax, Umin, Umax);
-
-%% Wizualizacja
-t = linspace(1,kk,kk);
-figure; 
-stairs(t,u,'LineWidth',1.5, Color='r');
-title('u - sterowanie'); 
-xlabel('k - number próbki');
-ylabel("Wartość sterowania")
-matlab2tikz ('zad4DMC_u_popraw.tex' , 'showInfo' , false)
-figure; 
-stairs(t,y,'LineWidth',1.5); 
-hold on;
-stairs(t,yzad,'LineWidth',1, 'LineStyle','--');
-title('Charakterystyki y,y_{zad}'); 
-xlabel('k - number próbki');
-ylabel('Wartość')
-legend("Wartość na wyjściu y", "Wartość zadana y_{zad}",Location="southeast")
-matlab2tikz ('zad4DMC_y_popraw.tex' , 'showInfo' , false)
-function [u, y, e] = regulator_DMC(Upp, Ypp, s,yzad, D, N, Nu, lambda, deltaumax, Umin, Umax)
+function [u, y, e] = dmcfunction(Upp, Ypp, s,yzad, D, N, Nu, lambda, deltaumax, Umin, Umax)
     % Inicjalizacja wektorów
     kk = length(yzad);
     u = zeros(1, kk);
